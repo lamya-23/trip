bb22e55e0a0b6f167ed49b6cc57feea6b748830d
ouardirhi <lamyaa.ouardirhi@gmail.com>
2020-04-27 12:42:50 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trip::Application.config.secret_token = '2a24ebb34d6bfbbd529b76b350bba63e31e5b29a2c34c0289c5aeb89802d78ca17dfda073c9036aff28699b5938b68a683df0411e62bd1a39b033aece0d42e4c'
